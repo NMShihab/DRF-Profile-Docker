2b5d547b4988391d02fc051b15ddfb088f057d0b
NMShihab <nmshihabislam@gmail.com>
2021-07-07 16:09:54 +0600

    User with token

--- code/userapi/urls.py
+++ code/userapi/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView
-    
-# )
-from .views import test_apiview,hello_world, MyTokenObtainPairView
+from .views import test_apiview,hello_world, MyTokenObtainPairView,getUserProfile,getAllProfile
+
 urlpatterns = [
     path("test",test_apiview,name="testapi"),
     path("hello",hello_world,name="hello"),
-    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair')
+    path('login/', MyTokenObtainPairView.as_view(), name='login'),
+    path("user/profile/",getUserProfile,name="userProfile"),
+    path("users",getAllProfile,name="users"),
 
     
 ]
--- code/userapi/views.py
+++ code/userapi/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse, response
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
+from .serializers import MyTokenObtainPairSerializer, UserSerializer,UserDataSerializer
 
-        # Add custom claims
-        token['name'] = user.username
-        # ...
 
-        return token
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user,many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getAllProfile(request):
+    users = User.objects.all()
+    serializer = UserDataSerializer(users,many=True)
+    return Response(serializer.data)
+
 
 @api_view(['GET','POST'])
 def hello_world(request):
